2af5efaac66fb669e7587eec8b48a67e9cc9817a
Michael Perez <mperez6473@gmail.com>
2020-06-02 16:20:28 -0500

    Have the comment sending to just posts with active IDs

--- posts/posts.js
+++ posts/posts.js
@@ -17,14 +17,14 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-    const item = req.body;
-    if (!item.title || !item.contents) {
+    const post = req.body;
+    if (!post.title || !post.contents) {
         res.status(400).json({
             errorMessage: "Please provide title and contents for the post.",
         });
-    } else if (item.title && item.contents) {
-        item.id = Date.now();
-        res.status(201).json(item);
+    } else if (post.title && post.contents) {
+        post.id = Date.now();
+        res.status(201).json(post);
     } else
         res.status(500).json({
             error: "There was an error while saving the post to the database",
@@ -34,14 +34,23 @@ router.post("/", (req, res) => {
 router.post("/:id/comments", (req, res) => {
     const id = req.params.id;
 
-    const item = req.body;
-    item.post_id = id;
+    const comment = req.body;
+    comment.post_id = id;
     Posts.findById(id).then((post) => {
         if (post.length > 0) {
-            res.status(200).json(post);
+            if (!comment.text) {
+                res.status(400).json({
+                    errorMessage: "Please provide text for the comment.",
+                });
+            } else res.status(201).json({ comment });
         } else if (post.length === 0) {
-            res.status(404).json({ message: "notFound" });
-        }
+            res.status(404).json({
+                message: "The post with the specified ID does not exist.",
+            });
+        } else
+            res.status(500).json({
+                error: "The posts information could not be retrieved.",
+            });
     });
 });
 
