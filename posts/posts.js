a56176e89b4466da563192fa746b80d89c993650
Michael Perez <mperez6473@gmail.com>
2020-06-02 17:22:28 -0500

    Have the items being deleted.

--- posts/posts.js
+++ posts/posts.js
@@ -87,4 +87,20 @@ router.post("/:id/comments", (req, res) => {
     });
 });
 
+router.delete("/:id", (req, res) => {
+    const id = req.params.id;
+    Posts.findById(id).then((post) => {
+        if (post.length === 0) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist.",
+            });
+        } else if (post) {
+            Posts.remove(id).then(
+                res.status(204).json({ message: "Post was deleted" })
+            );
+        } else
+            res.status(500).json({ error: "The post could not be removed." });
+    });
+});
+
 module.exports = router;
